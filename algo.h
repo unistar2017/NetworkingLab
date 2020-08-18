9622903aa55812714a4ff4833ea706a3e6a3e8ae
unistar2017 <69491775+unistar2017@users.noreply.github.com>
2020-08-18 14:47:49 +0900

    Add files via upload

---
+++ algo.h
@@ -0,0 +1,40 @@
+#ifndef __algo_h__
+#define __algo_h__
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE
+#endif
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/sendfile.h>
+#include <dlfcn.h>
+
+#include <unistd.h>
+#include <sys/uio.h>
+#include <sys/socket.h>
+#include <sys/time.h> 
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <time.h> 
+#include <math.h>
+#include <stdbool.h>
+#include <signal.h>
+#include <pthread.h>
+#include <signal.h>
+#include <time.h>
+#include <linux/tcp.h>  
+
+
+
+
+#ifdef __cplusplus
+}; // end of extern "C"
+#endif
+#endif
